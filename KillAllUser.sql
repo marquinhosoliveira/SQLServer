588418af270c4e6a0cde44d31f640a4c195295ea
MARCOS OLIVEIRA <31602136+marquinhosoliveira@users.noreply.github.com>
2021-10-11 21:07:42 -0300

    Delete KillAllUser.sql

--- KillAllUser.sql
+++
@@ -1,9 +0,0 @@
-USE [master];
-
-DECLARE @kill varchar(8000) = '';  
-SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';'  
-FROM sys.dm_exec_sessions
--- DBNAME coloque o nome do database para que seja derrubado todos os usuários de um database
-WHERE database_id  = db_id('DBNAME')
- 
-EXEC(@kill);
